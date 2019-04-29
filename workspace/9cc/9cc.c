5031d06253e3032cb8d4e2f6b1208b2362d7ee9f
sukesan1984 <takamikousuke@gmail.com>
2019-04-29 16:35:40 +0900

    add token and parser of 'return'

--- workspace/9cc/9cc.c
+++ workspace/9cc/9cc.c
@@ -6,6 +6,7 @@
 // トークンの型を表す値
 enum {
     TK_NUM = 256,
+    TK_RETURN,
     TK_IDENT,
     TK_EOF = 1,
 };
@@ -54,8 +55,9 @@ Vector* tokens;
 int pos = 0;
 
 enum {
-    ND_NUM = 256, // 整数のノードの型
-    ND_IDENT,
+    ND_NUM = 256,   // 整数のノードの型
+    ND_RETURN,      // returnのノード
+    ND_IDENT,       // 変数のノード
 };
 
 typedef struct Node {
@@ -104,12 +106,25 @@ int consume(int ty) {
     return 1;
 }
 
+int is_alnum(char c) {
+    return ('a' <= c && c <= 'z') ||
+           ('A' <= c && c <= 'Z') ||
+           ('0' <= c && c <= '9') ||
+           (c == '_');
+}
+
 // pが指している文字列をトークンに分割してtokensに保存する
 void tokenize(char *p) {
     while (*p) {
         // 空白文字列をスキップ
         if (isspace(*p)) {
             p++;
+            continue;
+        }
+
+        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
+            add_token(tokens, TK_RETURN, p);
+            p += 6;
             continue;
         }
 
@@ -158,12 +173,19 @@ void program() {
 }
 
 Node *stmt() {
-    Node *lhs = assign();
+    Node *node;
+    if (consume(TK_RETURN)) {
+        node = malloc(sizeof(Node));
+        node->ty = ND_RETURN;
+        node->lhs = assign();
+    } else {
+        node = assign();
+    }
     if (!consume(';')) {
         Token *t = tokens->data[pos];
         error("';'ではないトークンです: %s", t->input);
     }
-    return lhs;
+    return node;
 }
 
 Node *assign() {
