3725e1453ba7d20146c8963d431b058a8d31fa10
sukesan1984 <takamikousuke@gmail.com>
2019-05-04 19:29:33 +0900

    separate tokenize function to another file

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ build:
 	docker build -t sukesan1984/make_compiler .
 
 test:
-	docker run --rm sukesan1984/make_compiler  bash -c 'cd 9cc; make test'
+	make build
+	docker run --rm sukesan1984/make_compiler  bash -c 'cd 9cc; make clean && make test'
--- workspace/9cc/parse.c
+++ workspace/9cc/parse.c
@@ -1,9 +1,6 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "9cc.h"
-
 
 int pos = 0;
 int variables = 0;
@@ -12,12 +9,6 @@ void error(char *str, char *i){
     fprintf(stderr, "%s%s\n", str, i);
     exit(1);
 }
-
-void error_token(int i){
-    Token *t = tokens->data[i];
-    error("予期せぬトークンです:", t->input);
-}
-
 
 int consume(int ty) {
     Token *t = tokens->data[pos];
@@ -72,92 +63,6 @@ Node *new_node_for(int ty, Node *lhs, Node *lhs2, Node *lhs3, Node *rhs) {
     node->lhs3 = lhs3;
     node->rhs = rhs;
     return node;
-}
-
-int is_alnum(char c) {
-    return ('a' <= c && c <= 'z') ||
-           ('A' <= c && c <= 'Z') ||
-           ('0' <= c && c <= '9') ||
-           (c == '_');
-}
-
-int tokenize_comparable(Vector* tokens, int ty, char *p, char* token) {
-    int len = strlen(token);
-    if (strncmp(p, token, len) == 0) {
-        add_token(tokens, ty, p);
-        return 1;
-    }
-    return 0;
-}
-// pが指している文字列をトークンに分割してtokensに保存する
-void tokenize(char *p) {
-    while (*p) {
-        // 空白文字列をスキップ
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
-
-        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
-            add_token(tokens, TK_RETURN, p);
-            p += 6;
-            continue;
-        }
-
-        if (tokenize_comparable(tokens, TK_EQ, p, "==")) { p += 2; continue; };
-        if (tokenize_comparable(tokens, TK_NE, p, "!=")) { p += 2; continue; };
-        if (tokenize_comparable(tokens, TK_LE, p, "<=")) { p += 2; continue; };
-        if (tokenize_comparable(tokens, TK_GE, p, ">=")) { p += 2; continue; };
-
-        if (strncmp(p, "if", 2) == 0 && !is_alnum(p[2])) {
-            add_token(tokens, TK_IF, p);
-            p += 2;
-            continue;
-        }
-
-        if (strncmp(p, "while", 5) == 0 && !is_alnum(p[5])) {
-            add_token(tokens, TK_WHILE, p);
-            p += 5;
-            continue;
-        }
-
-        if (strncmp(p, "for", 3) == 0 && !is_alnum(p[3])) {
-            add_token(tokens, TK_FOR, p);
-            p += 3;
-            continue;
-        }
-
-        if (strchr("+-*/()=;{},<>", *p)) {
-            add_token(tokens, *p, p);
-            p++;
-            continue;
-        }
-
-        if (isdigit(*p)) {
-            Token * t = add_token(tokens, TK_NUM, p);
-            t->val = strtol(p, &p, 10);
-            continue;
-        }
-
-        if (is_alnum(*p)) {
-            char* init_p = p;
-            int len = 0;
-            // パースできる文字出るじゃない文字が出てくるまで
-            Token *t = add_token(tokens, TK_IDENT, p);
-            while(is_alnum(*p)){
-                len++;
-                p++;
-            }
-            char *name = (char *) malloc(len + 1);
-            strncpy(name, init_p, len);
-            t->name = name;
-            continue;
-        }
-
-        fprintf(stderr, "トークナイズできません: %s\n", p);
-        exit(1);
-    }
-    add_token(tokens, TK_EOF, p);
 }
 
 Vector *parse() {
