f224c8b455468ed5ef4865e02d850912043254d4
sukesan1984 <takamikousuke@gmail.com>
2019-05-09 18:14:24 +0900

    mv some utility functions to util.c

--- workspace/9cc/container.c
+++ workspace/9cc/container.c
@@ -4,48 +4,7 @@
 #include <string.h>
 #include "9cc.h"
 
-// エラーを報告するための関数
-void error(char *str, char *i){
-    fprintf(stderr, "%s%s\n", str, i);
-    exit(1);
-}
 
-// 可変長Vector
-// 任意蝶の入力をサポートする
-Vector *new_vector() {
-    Vector *vec = malloc(sizeof(Vector));
-    vec-> data = malloc(sizeof(void *) * 16);
-    vec->capacity = 16;
-    vec->len = 0;
-    return vec;
-}
-
-void vec_push(Vector *vec, void *elem) {
-    if (vec->capacity == vec->len) {
-        vec->capacity *= 2;
-        vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
-    }
-    vec->data[vec->len++] = elem;
-}
-
-Map *new_map() {
-    Map *map = malloc(sizeof(Map));
-    map->keys = new_vector();
-    map->vals = new_vector();
-    return map;
-}
-
-void map_put(Map *map, char *key, void *val) {
-    vec_push(map->keys, key);
-    vec_push(map->vals, val);
-}
-
-void *map_get(Map *map, char *key) {
-    for (int i = map->keys->len -1; i >= 0; i--)
-        if (strcmp(map->keys->data[i], key) == 0)
-            return map->vals->data[i];
-    return NULL;
-}
 
 int equal(int line, int expected, int actual) {
     if (expected == actual)
