f14c46fc47c38ee52c397fab893d367f923d5a8c
Sukesan1984 <takamikousuke@gmail.com>
2020-05-22 14:30:03 +0900

    Try to include <stdio.h>

--- workspace/9cc/test/sample.c
+++ workspace/9cc/test/sample.c
@@ -1,11 +1,5 @@
-int a = 5;
-int b = 2147483647;
-char *c;
+#include <stdio.h>
 int main() {
-    int x[2];
-    x[0] = 1;
-    x[1] = 2;
-    a = 3;
-    printf("a: %d, b: %d\n", a, b);
+    printf("hello world\n");
     return 0;
 }
