707bf2de9df49c9f5c316d9eaede982ba39a3bec
Sukesan1984 <takamikousuke@gmail.com>
2020-05-22 14:37:48 +0900

    Separate 9cc.h

--- workspace/9cc/9cc.h
+++ workspace/9cc/9cc.h
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE
+#include "struct.h"
 //#include <assert.h>
 void assert(int expression);
 #include <stdnoreturn.h>
@@ -9,115 +10,11 @@ void assert(int expression);
 #include <sys/stat.h>
 #include <stdarg.h>
 #include <ctype.h>
-#include <stdbool.h>
 #include <errno.h>
-// container.c
-typedef struct {
-    void **data;  // 実際のデータ
-    int capacity; // バッファの大きさ
-    int len;      // ベクタに追加済みの要素の個数。len == capacityのときにバッファがいっぱい、新たに要素を足す場合は、新たにバッファを確保して既存の要素をコピーし、dataポインタをすげ替える
-} Vector;
-
-typedef struct {
-    Vector *keys;
-    Vector *vals;
-} Map;
-
-// トークンの型
-typedef struct {
-    int ty;      // トークンの型
-    int val;     // tyがTK_NUMの場合,その数値
-    char *str;   // tyがTK_STRの場合はそのリテラル
-    char len;
-    char *name;  // tyがTK_IDENTの場合、その名前
-    char *input; // トークン文字列(エラーメッセージ用)
-
-    // For preprocessor
-    bool stringize;
-
-    // For error reporting
-    char *buf;
-    char *filename;
-    char *start;
-    char *end;
-} Token;
-
-typedef struct Type {
-    enum {INT, PTR, ARRAY, CHAR, VOID, STRUCT, FUNC} ty;
-    int size;
-    int align;
-
-    // Pointer
-    struct Type *ptr_to;
-
-    // Array
-    struct Type *array_of;
-    size_t array_size;
-
-    // Struct
-    Vector *members;
-
-    int offset;
-    // Function
-    struct Type *returning;
-} Type;
 
 Type *ary_of(Type *base, size_t size);
 Type *ptr_to(Type *base);
 
-typedef struct {
-    Type *ty;
-    int offset;
-    bool is_local;
-    char* data;
-    int len;
-    bool has_initial_value;
-    int val;
-    char *name;
-    bool is_extern;
-    bool is_static;
-} Var;
-
-typedef struct Node {
-    int op; // 演算子かTK_NUM
-    Type *ty;         // C type
-    struct Node *lhs; // 左辺(tyがTK_FORのときforの左)
-    struct Node *lhs2; //tyがTK_FORのときのforの真ん中
-    struct Node *lhs3; //tyがTK_FORのときのforの右
-    struct Node *rhs; // 右辺
-    struct Node *cond; // TK_IFのcondition
-    struct Node *if_body; // TK_IFのbody
-    struct Node *else_body; // TK_ELSEのbody
-    struct Node *body; // function definition
-    struct Node *init;
-    Vector *args;      // 関数の場合の引数が格納される
-    Vector *stmts;     // stmtの集合が入る
-    int val; // ty がTK_NUMの場合のみ使う
-    char* name;
-
-    int break_label;
-    int continue_label;
-
-    // For switch and case
-    Vector *cases;
-    int case_label;
-
-    // For break and continue
-    struct Node *target;
-
-    // Global variable
-    char *data;
-    int len;
-    bool has_initial_value;
-
-    int stacksize;
-
-    // Offset from BP or begining of a struct
-    int offset;
-
-    bool is_extern;
-    bool is_static;
-} Node;
 
 // トークンの型を表す値
 enum {
--- workspace/9cc/test/sample.c
+++ workspace/9cc/test/sample.c
@@ -1,5 +1,309 @@
-#include <stdio.h>
-int main() {
-    printf("hello world\n");
-    return 0;
+#include <struct.h>
+static Type int_ty = {INT, 4, 4, NULL, NULL, 0, NULL, 0, NULL};
+
+static int str_label;
+static Map *vars;
+Vector *globals;
+Vector *strings;
+static int stacksize;
+
+static Node *maybe_decay(Node *base, bool decay) {
+    if (!decay || base->ty->ty != ARRAY)
+        return base;
+    Node *node = calloc(1, sizeof(Node));
+    node->op = ND_ADDR;
+    node->ty = ptr_to(base->ty->array_of);
+    node->lhs = base;
+    return node;
+}
+
+static void check_lval(Node *node) {
+    int op = node->op;
+    if (op != ND_LVAR && op != ND_GVAR && op != ND_DEREF && op != ND_DOT)
+        error("not an lvaleu: %d (%s)", op, node->name);
+}
+
+Node *create_new_node(int ty, Node *lhs, Node *rhs) {
+    Node *node = malloc(sizeof(Node));
+    node->op = ty;
+    node->ty = lhs->ty;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+static Node* walk(Node *node, bool decay);
+
+Node *recreate_assgin_op(int ty, Node *lhs, Node *rhs) {
+    Node* new_rhs = create_new_node(ty, lhs, rhs);
+    return walk(create_new_node('=', lhs, new_rhs), true);
+}
+
+static Var *new_global(Type* ty, char *name, char *data, int len, bool is_extern, bool is_static) {
+    Var *var = calloc(1, sizeof(Var));
+    var->ty = ty;
+    var->is_local = false;
+    var->is_extern = is_extern;
+    var->is_static = is_static;
+    var->data = data;
+    var->len = len;
+    var->name = name;
+    return var;
+}
+
+static Node* walk(Node *node, bool decay) {
+    switch (node->op) {
+    case ND_NUM:
+        node->ty = &int_ty;
+        return node;
+    case ND_STR: {
+        char *name = format(".L.str%d", str_label++);
+        Var *var = new_global(node->ty, name, node->data, node->len, false, true);
+        vec_push(globals, var);
+        Node *ret = calloc(1, sizeof(Node));
+        ret->op = ND_GVAR;
+        ret->ty = node->ty;
+        ret->name = name;
+        return walk(ret, decay);
+    }
+    case ND_IDENT: {
+        Var *var = map_get(vars, node->name);
+        if (!var)
+            error("undefined variable: %s", node->name);
+        if (var->is_local) {
+            Node *ret = calloc(1, sizeof(Node));
+            ret->op = ND_LVAR;
+            ret->ty = var->ty;
+            ret->offset = var->offset;
+            ret->name = var->name;
+            return maybe_decay(ret, decay);
+        }
+
+        Node *ret = calloc(1, sizeof(Node));
+        ret->op = ND_GVAR;
+        ret->ty = var->ty;
+        ret->name = var->name;
+        return maybe_decay(ret, decay);
+    }
+
+    case ND_DOT:
+        node->lhs = walk(node->lhs, true);
+        if (node->lhs->ty->ty != STRUCT)
+            error("struct expected before '.'");
+        Type *ty = node->lhs->ty;
+        for (int i = 0; i < ty->members->len; i++) {
+            Node *m = ty->members->data[i];
+            if (strcmp(m->name, node->name))
+                continue;
+            node->ty = m->ty;
+            node->offset = m->ty->offset;
+            return maybe_decay(node, decay);
+        }
+        error("member missing: %s", node->name);
+    case ND_GVAR:
+        if (decay && node->ty->ty == ARRAY)
+            return maybe_decay(node, decay);
+        return node;
+    case ND_VARDEF:
+        roundup(stacksize, node->ty->align);
+        stacksize += node->ty->size;
+
+        node->offset = stacksize;
+
+        Var *var = calloc(1, sizeof(Var));
+        var->ty = node->ty;
+        var->offset = stacksize;
+        var->is_local = true;
+        var->name = node->name;
+        map_put(vars, node->name, var);
+        if (node->init)
+            node->init = walk(node->init, true);
+        return node;
+    case ND_IF:
+        node->cond= walk(node->cond, true);
+        node->if_body = walk(node->if_body, true);
+        if (node->else_body)
+            node->else_body = walk(node->else_body, true);
+        return node;
+    case '?':
+        node->cond = walk(node->cond, true);
+        node->if_body = walk(node->if_body, true);
+        node->else_body = walk(node->else_body, true);
+        node->ty = node->if_body->ty;
+        return node;
+    case ND_FOR:
+        node->lhs = walk(node->lhs, true);
+        if (node->lhs2)
+            node->lhs2 = walk(node->lhs2, true);
+        node->rhs = walk(node->rhs, true);
+        if (node->lhs3)
+            node->lhs3 = walk(node->lhs3, true);
+        return node;
+    case ND_WHILE:
+        node->lhs = walk(node->lhs, true);
+        node->rhs = walk(node->rhs, true);
+        return node;
+    case ND_MUL_EQ:
+        return recreate_assgin_op('*', node->lhs, node->rhs);
+    case ND_DIV_EQ:
+        return recreate_assgin_op('/', node->lhs, node->rhs);
+    case ND_MOD_EQ:
+        return recreate_assgin_op('%', node->lhs, node->rhs);
+    case ND_ADD_EQ:
+        return recreate_assgin_op('+', node->lhs, node->rhs);
+    case ND_SUB_EQ:
+        return recreate_assgin_op('-', node->lhs, node->rhs);
+    case ND_SHL_EQ:
+        return recreate_assgin_op(ND_LSHIFT, node->lhs, node->rhs);
+    case ND_SHR_EQ:
+        return recreate_assgin_op(ND_RSHIFT, node->lhs, node->rhs);
+    case ND_BITAND_EQ:
+        return recreate_assgin_op('&', node->lhs, node->rhs);
+    case ND_XOR_EQ:
+        return recreate_assgin_op('^', node->lhs, node->rhs);
+    case ND_BITOR_EQ:
+        return recreate_assgin_op('|', node->lhs, node->rhs);
+    case '=':
+        node->lhs = walk(node->lhs, false);
+        node->rhs = walk(node->rhs, true);
+        node->ty = node->lhs->ty;
+        return node;
+    case ND_SWITCH:
+        node->cond = walk(node->cond, true);
+        node->body = walk(node->body, true);
+        return node;
+    case ND_CASE:
+        node->body = walk(node->body, true);
+        return node;
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+    case '<':
+    case '>':
+    case ND_LOGAND:
+    case ND_LOGOR:
+    case '|':
+    case '^':
+    case '&':
+    case ND_LSHIFT:
+    case ND_RSHIFT:
+    case ND_EQ:
+    case ND_NE:
+    case ND_LE:
+        node->lhs = walk(node->lhs, true);
+        node->rhs = walk(node->rhs, true);
+        node->ty = node->lhs->ty;
+        return node;
+    case ND_DEREF:
+        node->lhs = walk(node->lhs, true);
+        if(node->lhs->ty->ty == ARRAY)
+            fprintf(stderr, "operand is ARRAY\n");
+
+        if(node->lhs->ty->ty != PTR)
+            error("operand must be a pointer");
+
+        if(node->lhs->ty->ptr_to->ty == VOID)
+            error("cannot dereference void pointer");
+
+        node->ty = node->lhs->ty->ptr_to; // *p の場合 tyはptr_of
+        return maybe_decay(node, decay);
+    case ND_ADDR:
+        node->lhs = walk(node->lhs, true);
+        check_lval(node->lhs);
+        node->ty = ptr_to(node->lhs->ty);
+        return node;
+    case ND_PREINC:
+    case ND_PREDEC:
+    case ND_POSTINC:
+    case ND_POSTDEC:
+    case ND_NEG:
+    case '~':
+    case '!':
+        node->lhs = walk(node->lhs, true);
+        node->ty  = node->lhs->ty;
+        return node;
+    case ND_RETURN:
+        node->lhs = walk(node->lhs, true);
+        return node;
+    case ND_BREAK:
+    case ND_CONTINUE:
+        return node;
+    case ND_CALL: {
+        Var *var = map_get(vars, node->name);
+        if (var && var->ty->ty == FUNC) {
+            node->ty = var->ty->returning;
+        } else {
+            fprintf(stderr, "bad function: %s\n", node->name);
+            node->ty = &int_ty;
+        }
+        for (int i = 0; i < node->args->len; i++)
+            node->args->data[i] = walk(node->args->data[i], true);
+        return node;
+    }
+    case ND_FUNC:
+        for (int i = 0; i < node->args->len; i++)
+            node->args->data[i] = walk(node->args->data[i], true);
+        node->body = walk(node->body, true);
+        return node;
+    case ND_COMP_STMT:
+        for (int i = 0; i < node->stmts->len; i++)
+            node->stmts->data[i] = walk(node->stmts->data[i], true);
+        return node;
+    case ND_EXPR_STMT:
+        node->lhs = walk(node->lhs, true);
+        return node;
+    case ND_STMT_EXPR:
+        node->lhs = walk(node->lhs, true);
+        return node;
+    case ND_SIZEOF:
+        node->lhs = walk(node->lhs, false);
+        node->op = ND_NUM;
+        node->val = node->lhs->ty->size;
+        return node;
+    case ND_NULL:
+        return node;
+    case ',':
+        node->lhs = walk(node->lhs, true);
+        node->rhs = walk(node->rhs, true);
+        node->ty = node->rhs->ty;
+        return node;
+    default:
+        assert(0 && "unknown node type");
+        return NULL;
+    }
+}
+
+void sema(Vector *nodes) {
+    str_label = 0;
+    globals = new_vector();
+    vars = new_map();
+    for (int i = 0; i < nodes->len; i++) {
+        Node *node = nodes->data[i];
+
+        // Global Variables
+        if (node->op == ND_VARDEF) {
+            Var *var = new_global(node->ty, node->name, node->data, node->len, node->is_extern, node->is_static);
+            if (node->has_initial_value) {
+                var->has_initial_value = true;
+                var->val = node->val;
+            }
+
+            vec_push(globals, var);
+            map_put(vars, node->name, var);
+            continue;
+        }
+
+        assert(node->op == ND_DECL || node->op == ND_FUNC);
+        Var *var = new_global(node->ty, node->name, "", 0, node->is_extern, node->is_static);
+        map_put(vars, node->name, var);
+        if (node->op == ND_DECL)
+            continue;
+
+        stacksize = 0;
+        node = walk(node, true);
+        node->stacksize = stacksize;
+    }
+    fprintf(stderr, "sema done\n");
 }
