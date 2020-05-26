eb4388979ad4b09eb2130987ba564f80696cacd2
Sukesan1984 <takamikousuke@gmail.com>
2020-05-26 15:34:27 +0900

    Add struct initializers for global variables

--- workspace/9cc/test/sample.c
+++ workspace/9cc/test/sample.c
@@ -1,8 +1,11 @@
 char *g6[] = {"abc", "efg"};
 char *g5 = "hij";
+struct { char a; int b; } g9[2] = {{1, 2}, {3, 4}};
 //int g[] = {1, 2, 3};
 int main () {
-    printf("%s\n", g5);
+    printf("%d\n", g9[0].a);
+    printf("%d\n", g9[0].b);
+    //printf("%s\n", g5);
     //printf("%s\n", g6[0]);
     //printf("%s\n", g6[1]);
     //printf("%d\n", g[0]);
