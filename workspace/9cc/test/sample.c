8f211d354dea61ca1910348034ccc73d4bbb504c
Sukesan1984 <takamikousuke@gmail.com>
2020-06-14 13:55:24 +0900

    Support struct assignment

--- workspace/9cc/test/sample.c
+++ workspace/9cc/test/sample.c
@@ -1,17 +1,29 @@
+//#include <stdio.h> //#include <stdlib.h>
+
+typedef struct PlaceHolder {
+    int val;
+} PlaceHolder;
+
+void    nop() {}
+extern void *calloc (int __nmemb, int __size);
 
 int main() {
-  int x = 0;
-  switch (3) {
-      case 1:
-          x = 1;
-          break;
-      default:
-          x= 4;
-          break;
-      case 2:
-          x = 2;
-          break;
-  }
-  printf("%d\n", x);
-  return 0;
+    struct tag {
+        char a;
+        int b;
+    } x;
+    struct tag *p = &x;
+    x.a = 3;
+    x.b = 5;
+    nop();
+    printf("%d\n", p->a + p->b);
+
+    nop();
+    PlaceHolder *placeholder = calloc(1, sizeof(PlaceHolder));
+    nop();
+    PlaceHolder replaced;
+    replaced.val = 3;
+    *placeholder = replaced;
+    printf("val: %d\n", placeholder->val);
+    return 0;
 }
