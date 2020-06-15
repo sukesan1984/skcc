5916e07f60084f08229e2426280ab4a1f325963a
Sukesan1984 <takamikousuke@gmail.com>
2020-06-15 21:27:05 +0900

    TK_STR len should be larger than char

--- workspace/9cc/test/sample.c
+++ workspace/9cc/test/sample.c
@@ -1,6 +1,7 @@
 
 int main() {
-    int a = !1;
-    printf("%d\n", a);
+    printf("  sub rax, %d   # raxを%sのoffset:%d分だけ押し下げたアドレスが%sの変数のアドレス。それをraxに保存)\n", 10, "hoge", 10, "hoge");  // raxをoffset文だけ押し下げ（nameの変数のアドレスをraxに保存)
+    //printf("  %d %sのoffset:%d%s\n", 10, "hoge", 10, "hoge");  // raxをoffset文だけ押し下げ（nameの変数のアドレスをraxに保存)
+    //printf("  %d %sのoffset:%d%s\n", 10, "hoge", 10, "hoge");  // raxをoffset文だけ押し下げ（nameの変数のアドレスをraxに保存)
     return 0;
 }
